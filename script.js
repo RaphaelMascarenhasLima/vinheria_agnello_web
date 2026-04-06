f048e1211155f5e30c1eddfbbe2f213bb013ff4c
RaphaelMascarenhasLima <raphaelmasc@gmail.com>
2026-04-05 21:18:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+alert("Bem-vindo a Vinheria Agnello!")
+var nome = prompt("Qual é o seu nome?")
+alert("Olá, " + nome )
+var idade = prompt("Qual é a sua idade?")
+if (idade >= 18) {
+    alert("Você é maior de idade e pode acessar o site.")
+} else {
+    alert("Desculpe, você precisa ser maior de idade para acessar o site.")
+}
+var NomeDoVinho = prompt("Qual o nome do vinho que você deseja comprar?")
+var TipoDEeDoVinho = prompt("Qual tipo de vinho que você deseja comprar?(Tinto,Branco ou Rosé)")
+var Quantidade = prompt("Quantas garrafas você deseja comprar?")
+alert("Você selecionou "+  Quantidade +  NomeDoVinho)
+ alert("Cadastro Finalizado! Veja os detalhes no console")
+ alert("Veja os detalhes do vinho no console por favor")
+
+
+
+
+
+console.log("O nome do usuário é: " + nome)
+console.log("A idade do usuário é: " + idade)
+console.log("O nome do vinho selecionado é: " + NomeDoVinho)
+console.log("O tipo do vinho selecionado é: " + TipoDEeDoVinho)
+console.log("A quantidade de garrafas selecionada é: " + Quantidade)
